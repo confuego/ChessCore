56c5c5a09626bdbca0f76fdd02a116e1c0558393
Wesley <wes.chappell10@gmail.com>
2018-08-13 19:37:09 -0400

    - use is type assertion

--- ChessTests/BoardTests.cs
+++ ChessTests/BoardTests.cs
@@ -8,41 +8,41 @@ namespace ChessTests {
       var board = BoardFactory.Create();
 
       Assert.NotNull(board);
-      Assert.True(board.Get(0, 0)is Rook);
-      Assert.True(board.Get(0, 1)is Knight);
-      Assert.True(board.Get(0, 2)is Bishop);
-      Assert.True(board.Get(0, 3)is King);
-      Assert.True(board.Get(0, 4)is Queen);
-      Assert.True(board.Get(0, 5)is Bishop);
-      Assert.True(board.Get(0, 6)is Knight);
-      Assert.True(board.Get(0, 7)is Rook);
+      Assert.IsType<Rook>(board.Get(0, 0));
+      Assert.IsType<Knight>(board.Get(0, 1));
+      Assert.IsType<Bishop>(board.Get(0, 2));
+      Assert.IsType<King>(board.Get(0, 3));
+      Assert.IsType<Queen>(board.Get(0, 4));
+      Assert.IsType<Bishop>(board.Get(0, 5));
+      Assert.IsType<Knight>(board.Get(0, 6));
+      Assert.IsType<Rook>(board.Get(0, 7));
 
-      Assert.True(board.Get(1, 0)is Pawn);
-      Assert.True(board.Get(1, 1)is Pawn);
-      Assert.True(board.Get(1, 2)is Pawn);
-      Assert.True(board.Get(1, 3)is Pawn);
-      Assert.True(board.Get(1, 4)is Pawn);
-      Assert.True(board.Get(1, 5)is Pawn);
-      Assert.True(board.Get(1, 6)is Pawn);
-      Assert.True(board.Get(1, 7)is Pawn);
+      Assert.IsType<Pawn>(board.Get(1, 0));
+      Assert.IsType<Pawn>(board.Get(1, 1));
+      Assert.IsType<Pawn>(board.Get(1, 2));
+      Assert.IsType<Pawn>(board.Get(1, 3));
+      Assert.IsType<Pawn>(board.Get(1, 4));
+      Assert.IsType<Pawn>(board.Get(1, 5));
+      Assert.IsType<Pawn>(board.Get(1, 6));
+      Assert.IsType<Pawn>(board.Get(1, 7));
 
-      Assert.True(board.Get(7, 0)is Rook);
-      Assert.True(board.Get(7, 1)is Knight);
-      Assert.True(board.Get(7, 2)is Bishop);
-      Assert.True(board.Get(7, 3)is King);
-      Assert.True(board.Get(7, 4)is Queen);
-      Assert.True(board.Get(7, 5)is Bishop);
-      Assert.True(board.Get(7, 6)is Knight);
-      Assert.True(board.Get(7, 7)is Rook);
+      Assert.IsType<Rook>(board.Get(7, 0));
+      Assert.IsType<Knight>(board.Get(7, 1));
+      Assert.IsType<Bishop>(board.Get(7, 2));
+      Assert.IsType<King>(board.Get(7, 3));
+      Assert.IsType<Queen>(board.Get(7, 4));
+      Assert.IsType<Bishop>(board.Get(7, 5));
+      Assert.IsType<Knight>(board.Get(7, 6));
+      Assert.IsType<Rook>(board.Get(7, 7));
 
-      Assert.True(board.Get(6, 0)is Pawn);
-      Assert.True(board.Get(6, 1)is Pawn);
-      Assert.True(board.Get(6, 2)is Pawn);
-      Assert.True(board.Get(6, 3)is Pawn);
-      Assert.True(board.Get(6, 4)is Pawn);
-      Assert.True(board.Get(6, 5)is Pawn);
-      Assert.True(board.Get(6, 6)is Pawn);
-      Assert.True(board.Get(6, 7)is Pawn);
+      Assert.IsType<Pawn>(board.Get(6, 0));
+      Assert.IsType<Pawn>(board.Get(6, 1));
+      Assert.IsType<Pawn>(board.Get(6, 2));
+      Assert.IsType<Pawn>(board.Get(6, 3));
+      Assert.IsType<Pawn>(board.Get(6, 4));
+      Assert.IsType<Pawn>(board.Get(6, 5));
+      Assert.IsType<Pawn>(board.Get(6, 6));
+      Assert.IsType<Pawn>(board.Get(6, 7));
 
       for (byte i = 2; i < 6; i++) {
         for (byte j = 0; j < 8; j++) {
@@ -58,7 +58,7 @@ namespace ChessTests {
       board.Move(0, 0, 2, 0);
 
       Assert.Null(board.Get(0, 0));
-      Assert.True(board.Get(2, 0)is Rook);
+      Assert.IsType<Rook>(board.Get(2, 0));
     }
 
   }
