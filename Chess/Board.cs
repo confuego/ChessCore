c9ffc90f5ae21225c88ffa0053425ccbadf1ec6b
Wesley <wes.chappell10@gmail.com>
2018-08-13 23:51:09 -0400

    - use bit shifting to divide instead of actual division

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -7,19 +7,6 @@ namespace Chess {
   public class Board {
     public byte[] Buffer;
     public int Offset;
-    public Board() {
-      Buffer = (byte[])BUFFER.Clone();
-      Offset = 0;
-    }
-
-    public void Move(byte x, byte y, byte toX, byte toY) {
-      var currPiece = Board.Get(this, x, y);
-      var placeToMove = Board.Get(this, toX, toY);
-      if (currPiece != null && currPiece.CanMove(toX, toY, this)) {
-        Board.Clear(this, x, y);
-        Board.Set(currPiece.Type, currPiece.Color, this, toX, toY);
-      }
-    }
 
     public static byte[] BUFFER = new byte[] {
       53,
@@ -56,11 +43,25 @@ namespace Chess {
       219
     };
 
+    public Board() {
+      Buffer = (byte[])BUFFER.Clone();
+      Offset = 0;
+    }
+
+    public void Move(byte x, byte y, byte toX, byte toY) {
+      var currPiece = Board.Get(this, x, y);
+      var placeToMove = Board.Get(this, toX, toY);
+      if (currPiece != null && currPiece.CanMove(toX, toY, this)) {
+        Board.Clear(this, x, y);
+        Board.Set(currPiece.Type, currPiece.Color, this, toX, toY);
+      }
+    }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Set(PieceType type, PieceColor color, Board b, byte x, byte y) {
       var gameIndex = x * 8 + y;
       var loc = gameIndex == 0 || gameIndex % 2 == 0 ? 4 : 0;
-      var index = (byte)Math.Floor((decimal)gameIndex / 2) + b.Offset;
+      var index = gameIndex >> 1 + b.Offset;
       var bytesToEncode = ((byte)type) | ((byte)color) << 3;
       b.Buffer[index] |= (byte)(bytesToEncode << loc);
     }
@@ -69,7 +70,7 @@ namespace Chess {
     public static void Clear(Board b, byte x, byte y) {
       var gameIndex = x * 8 + y;
       var mask = gameIndex == 0 || gameIndex % 2 == 0 ? 15 : 240;
-      var index = (byte)Math.Floor((decimal)gameIndex / 2) + b.Offset;
+      var index = gameIndex >> 1 + b.Offset;
       b.Buffer[index] &= (byte)mask;
     }
 
@@ -77,10 +78,10 @@ namespace Chess {
     public static Piece Get(Board b, byte x, byte y) {
       var gameIndex = x * 8 + y;
       var loc = gameIndex == 0 || gameIndex % 2 == 0 ? 4 : 0;
-      var index = (byte)Math.Floor((decimal)gameIndex / 2) + b.Offset;
-      var bytesToDecode = b.Buffer[index] >> loc;
-      var pieceType = bytesToDecode & 7;
-      var color = (PieceColor)((bytesToDecode >> 3) & 1);
+      var index = gameIndex >> 1 + b.Offset;
+      var nibbleToDecode = b.Buffer[index] >> loc;
+      var pieceType = nibbleToDecode & 7;
+      var color = (PieceColor)((nibbleToDecode >> 3) & 1);
 
       switch ((PieceType)pieceType) {
         case PieceType.King:
